4cd1e2ea3607c6f28160917c122a75c620482713
aaron <2529619036@qq.com>
2025-03-04 22:43:48 +0800

    修改http返回结构

--- httpx/http_response.go
+++ httpx/http_response.go
@@ -1,17 +1,17 @@
 package httpx
 
-import "github.com/labstack/echo"
+import (
+	"github.com/labstack/echo"
+)
 
 type HttpResponse struct {
 	StatusCode StatusCode  `json:"status_code"`
-	Msg        string      `json:"msg"`
 	Data       interface{} `json:"data"`
 }
 
-func JsonResponse(ctx echo.Context, statusCode StatusCode, msg *string, data ...interface{}) error {
+func JsonResponse(ctx echo.Context, statusCode StatusCode, data ...interface{}) error {
 	return ctx.JSON(statusCode.Int(), HttpResponse{
 		statusCode,
-		*msg,
 		data,
 	})
 }
